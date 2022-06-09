bf9b1923cd4e092c52664cdb8b7a606181b5f64d
Ema-Rodriguez <83190827+Ema-Rodriguez@users.noreply.github.com>
2022-06-09 00:11:56 -0300

    Se realizo una limpieza de las importaciones no utilizadas

--- app/src/main/java/com/peakycoders/filmy/ui/home/HomeActivity.kt
+++ app/src/main/java/com/peakycoders/filmy/ui/home/HomeActivity.kt
@@ -1,34 +1,28 @@
 package com.peakycoders.filmy.ui.home
 
-import android.content.Context
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.*
-import androidx.compose.foundation.interaction.InteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.material3.FloatingActionButtonDefaults.elevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.peakycoders.filmy.R
 import com.peakycoders.filmy.entities.TransferMovie
 import com.peakycoders.filmy.entities.models.Movie
 import com.peakycoders.filmy.ui.details.DetailsActivity
@@ -48,7 +42,6 @@ class HomeActivity : ComponentActivity() {
             val isLoading = homeViewModel.isLoading.value
 
             FilmyTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
